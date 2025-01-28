9a3a3538e27c80009dde907625f5fa20d277a21c
Alejandro Vasconcellos <alejandro.vasconcellosnoailles@atomlearning.co.uk>
2025-01-28 16:24:41 +0000

    added new plugin and keymap

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -10,8 +10,12 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open [D]iagnostics [L]ist" })
 
--- Buffers
-vim.keymap.set("n", "<leader>q", function() vim.cmd("bd") end, { desc = "[Q]uit buffer" })
+-- Copy path
+vim.keymap.set("n", "<leader>yp", function()
+	vim.cmd('let @" = expand("%")')
+end, { desc = "[Y]ank file [P]ath" })
 
--- Inserts new line
-vim.keymap.set("n", "<Enter>", "i<Enter><Esc>")
+-- Buffers
+vim.keymap.set("n", "<leader>q", function()
+	vim.cmd("bd")
+end, { desc = "[Q]uit buffer" })
