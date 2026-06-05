aadfb2a20bcf8cd66200a20be4453ab4923399dc
Alejandro Vasconcellos <vasconalex17@gmail.com>
2026-06-05 10:12:32 +0100

    replaced deprecated API

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ require("lazy").setup({ import = "custom/plugins" }, {
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 	group = highlight_group,
 	pattern = "*",
